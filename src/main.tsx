df9d852e0f2517cc8a6c85f602f9bf14944a6c18
gabriel <gb.ribeiropedroso@gmail.com>
2025-08-24 16:36:15 -0300

    May the force be with you

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from './App'
+import About from "./pages/about";
+import './tails.css'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const root : any = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>,
+);
---
+++ src/pages/about.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+
+
+function About() {
+
+  return (
+    <div className='border'>
+      Welcome to the Jungle
+      <br></br>
+      <Button variant="contained">Contained</Button>
+    </div>
+  )
+}
+
+export default About
